6320fbc2c6375f0bc0bbaba26c81e583e4c1b16b
Fernando Luján Martínez <8flm95@gmail.com>
2020-03-23 12:33:42 +0100

    terminado y funcionando la lista recursiva

--- src/exercices/listarecursiva/ListaRecursiva.scala
+++ src/exercices/listarecursiva/ListaRecursiva.scala
@@ -13,8 +13,9 @@ abstract class ListaRecursiva[+A]{
   def imprimirElementos: String
   override def toString: String = "[" + imprimirElementos + "]"
   def mapear[B](transformador: Transformador[A, B]): ListaRecursiva[B]
-  def filtrar[T](predicado: Predicado[T]): ListaRecursiva[A]
-  def mapaTransformar[B](transformador: Transformador[A, ListaRecursiva[B]]): ListaRecursiva[C]
+  def filtrar(predicado: Predicado[A]): ListaRecursiva[A]
+  def mapaTransformar[B](transformador: Transformador[A, ListaRecursiva[B]]): ListaRecursiva[B]
+  def ++[B >: A](listaRecursiva: ListaRecursiva[B]): ListaRecursiva[B]
 }
 
 trait Predicado[-T] {
@@ -32,9 +33,10 @@ object Vacio extends ListaRecursiva[Nothing] {
   override def esVacia: Boolean = true
   override def insertar[B >: Nothing](elementoAInsertar: B): ListaRecursiva[B] = new Elemento[B](elementoAInsertar, this)
   override def imprimirElementos: String = ""
-  override def mapear[B](transformador: Transformador[Nothing]): ListaRecursiva[Nothing] = throw new NoSuchElementException
-  override def filtrar[T](predicado: Predicado[T]): ListaRecursiva[Nothing] = ???
-  override def mapaTransformar[B](transformador: Transformador[Nothing]): ListaRecursiva[B] = ???
+  override def mapear[B](transformador: Transformador[Nothing, B]): ListaRecursiva[B] = Vacio
+  override def filtrar(predicado: Predicado[Nothing]): ListaRecursiva[Nothing] = Vacio
+  override def mapaTransformar[B](transformador: Transformador[Nothing, ListaRecursiva[B]]): ListaRecursiva[B] = Vacio
+  override def ++[B >: Nothing](listaRecursiva: ListaRecursiva[B]): ListaRecursiva[B] = listaRecursiva
 }
 
 class Elemento [+A] (elemento: A, listaRecursiva: ListaRecursiva[A]) extends ListaRecursiva[A] {
@@ -46,31 +48,34 @@ class Elemento [+A] (elemento: A, listaRecursiva: ListaRecursiva[A]) extends Lis
   override def imprimirElementos: String =
     if(listaRecursiva.esVacia) "" + elemento
     else elemento + " " + listaRecursiva.imprimirElementos
-  override def mapear(transformador: Transformador[A]): ListaRecursiva[A] = mapearLista(transformador, this)
-  override def filtrar[T](predicado: Predicado[T]): ListaRecursiva[A] = ???
-    //if(predicado.test(elemento))
-  override def mapaTransformar(transformador: Transformador[A]): ListaRecursiva[A] = ???
 
-  def mapearLista[B](transformador: Transformador[B], listaRecursiva: ListaRecursiva[B]): ListaRecursiva[B] = {
-
-    if(listaRecursiva.esVacia) new Elemento[B](transformador.transformar(listaRecursiva.cabeza), Vacio)
-    else new Elemento[B](transformador.transformar(listaRecursiva.cabeza), mapearLista(transformador, listaRecursiva.cola))
+  override def filtrar(predicado: Predicado[A]): ListaRecursiva[A] = {
+    if(predicado.test(cabeza)) new Elemento(cabeza, cola.filtrar(predicado))
+    else cola.filtrar(predicado)
   }
+  override def mapear[B](transformador: Transformador[A, B]): ListaRecursiva[B] = {
+    new Elemento(transformador.transformar(cabeza), cola.mapear(transformador))
+  }
+  override def mapaTransformar[B](transformador: Transformador[A, ListaRecursiva[B]]): ListaRecursiva[B] =
+    transformador.transformar(cabeza) ++ cola.mapaTransformar(transformador)
+
+  override def ++[B >: A](listaRecursiva: ListaRecursiva[B]): ListaRecursiva[B] = new Elemento(cabeza, cola ++ listaRecursiva)
 }
 
 class PredicadoImpar extends Predicado[Int] {
-  override def test(elementoATestear: Int): Boolean = elementoATestear%2 == 0
+  override def test(elementoATestear: Int): Boolean = elementoATestear % 2 == 0
 }
 
-class duplicarValorTransformador extends Transformador[Int] {
-  override def transformar(elementoATransformar: Int): Int = elementoATransformar * 2
+class DuplicarValorTransformador extends Transformador[Int, ListaRecursiva[Int]] {
+  override def transformar(elementoATransformar: Int): ListaRecursiva[Int] = new Elemento(elementoATransformar , new Elemento(elementoATransformar + 1, Vacio))
 }
 
 object Test extends App {
 
   val lista1 = new Elemento[Int](1, new Elemento[Int](2, new Elemento[Int](3, Vacio)))
-  val transformador = new duplicarValorTransformador()
-  val lista2 = lista1.mapear(transformador)
+  val lista2 = lista1.mapear(new DuplicarValorTransformador())
   println(lista1.toString)
   println(lista2.toString)
+  println(lista1.filtrar(new PredicadoImpar()).toString)
+  println(lista1.mapaTransformar(new DuplicarValorTransformador()))
 }
